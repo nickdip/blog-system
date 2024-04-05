ade6450c746abf37b06a40275dc290342e376c42
Nick Diplos <nickd321@gmail.com>
2024-04-05 15:06:59 +0100

    Added BlogPosts Controller

---
+++ blog-system/Controllers/BlogPostsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using blog_system.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace blog_system.Controllers
+{
+    public class BlogPostsController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // GET: /blogposts/random
+        public IActionResult Random()
+        {
+            BlogPost b = new BlogPost() { Title = "My First Post", Body = "HELLO" };
+            return View(b);
+        }
+    }
+}
+
